d20d52e35323f89d732b84d31c277cb784be0d1f
keenobserver27 <brylle_barriga@yahoo.com>
2017-07-01 06:38:25 +0800

    after seeding feature table

---
+++ Controllers/Resources/FeatureResource.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace vega.Controllers.Resources
+{
+    public class FeatureResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
